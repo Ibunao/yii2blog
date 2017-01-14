54bb516daaa221d76e87b6ba9de18452809871b9
BUNAO <275122510@qq.com>
2017-01-14 10:29:23 +0800

    路由的美化
    路由的美化

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,11 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        //url美化配置
+        'urlManager' => [
+            'enablePrettyUrl' => true,//开启美化
+            'showScriptName' => false,//关闭脚本文件
+            // 'suffix' => '.html',//添加后缀
+        ],
     ],
 ];
